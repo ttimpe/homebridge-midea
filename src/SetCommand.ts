4ac68fbd2bde0c1fe904e94b411ccd7398efac03
Tobias <tobias@tim.pe>
2020-06-27 13:20:29 +0200

    Testing byte 0x0e

--- src/SetCommand.ts
+++ src/SetCommand.ts
@@ -78,9 +78,9 @@ export default class SetCommand extends BaseCommand {
     }
 
    get useFahrenheit() {
-       return this.data[0x12] > 1
+       return this.data[0x0e] > 1
     }
     set useFahrenheit(useFahrenheit : boolean) {
-        this.data[0x12] = useFahrenheit ? 0x01 : 0;
+        this.data[0x0e] = useFahrenheit ? 0x01 : 0;
     }
 }
