fa02fe42cbf2d70d87530c71bdea07ae97197f3a
Tobias <tobias@tim.pe>
2020-07-03 18:07:50 +0200

    Changed test

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-screen -d -m -S homebridge
+homebridge -D &
 sleep 10
+pgrep -x homebridge >> /dev/null && exit 0 || exit 1
 if ! screen -list | grep -q 'homebridge'; then
 	echo "Error"
 	exit -1
 else
-	killall screen
+	killall homebridge
 	exit 0
 fi
