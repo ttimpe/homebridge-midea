8e96156dbc7a61d87264bc636a5c29ae57a2897f
Tobias <tobias@tim.pe>
2020-07-03 23:16:41 +0200

    Will this work?

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,7 @@ homebridge -D &
 sleep 10
 which pgrep
 ps aux | grep homebridge
-if pgrep -x homebridge >/dev/null
+if pgrep 'homebridge' >/dev/null
 then
 	echo "Homebrige is still running, success"
 	pkill homebridge
